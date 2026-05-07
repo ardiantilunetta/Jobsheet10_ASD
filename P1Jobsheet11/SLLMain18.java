4217233b4d2a763dbc2c921f31524e4259284759
Nabila Ardianti Lunetta <nabilaardianti17@gmail.com>
2026-05-07 22:43:58 +0700

    modifikasi pertanyaan 3 praktikum 1

--- P1Jobsheet11/SLLMain18.java
+++ P1Jobsheet11/SLLMain18.java
@@ -1,18 +1,34 @@
+import java.util.Scanner;
+
 public class SLLMain18 {
     public static void main(String[] args) {
-        SingleLinkedList18 sll = new SingleLinkedList18();
-        Mahasiswa18 mhs1 = new Mahasiswa18("22001", "Andi", "TI-1A", 3.75);
-        Mahasiswa18 mhs2 = new Mahasiswa18("22002", "Budi", "TI-1B", 3.60);
-        Mahasiswa18 mhs3 = new Mahasiswa18("22003", "Citra", "TI-1C", 3.90);
-        Mahasiswa18 mhs4 = new Mahasiswa18("22004", "Dina", "TI-1D", 3.80);
 
-        sll.print();
-        sll.addFirst(mhs4);
-        sll.print();
-        sll.addLast(mhs1);
-        sll.print();
-        sll.insertAfter("Dirga", mhs3);
-        sll.insertAt(2, mhs2);
-        sll.print();
+        Scanner sc = new Scanner(System.in);
+        SingleLinkedList18 sll = new SingleLinkedList18();
+
+        for (int i = 1; i <= 4; i++) {
+
+            System.out.println("Data Mahasiswa ke-" + i);
+
+            System.out.print("NIM   : ");
+            String nim = sc.nextLine();
+
+            System.out.print("Nama  : ");
+            String nama = sc.nextLine();
+
+            System.out.print("Kelas : ");
+            String kelas = sc.nextLine();
+
+            System.out.print("IPK   : ");
+            double ipk = sc.nextDouble();
+            sc.nextLine();
+
+            Mahasiswa18 mhs = new Mahasiswa18(nim, nama, kelas, ipk);
+
+            sll.addLast(mhs);
+
+            sll.print();
+            System.out.println();
+        }
     }
 }
